d0c0d64389dc450c28d7cce789d9a874eee7e0e2
xiaoyuit <forxiaoyu@qq.com>
2019-04-24 09:01:03 +0800

    更新初始化脚本

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#替换阿里云源
+# 替换阿里云源
 sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g;s/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
-#开启密码登录并开启root账号登录
+# 开启密码登录并开启root账号登录
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g;s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo service ssh restart > /dev/null 2>&1
-#设置root账号密码
+# 设置root账号密码
 echo root:vagrant | sudo chpasswd root
-#安装docker
+# 安装docker
 sudo curl -fsSL "https://get.docker.com/" | bash -s -- --mirror Aliyun > /dev/null 2>&1
 sudo mkdir -p /etc/docker/
 sudo cat > /etc/docker/daemon.json <<EOF
@@ -24,11 +24,11 @@ sudo cat > /etc/docker/daemon.json <<EOF
   }
 }
 EOF
-#开启docker远程api
+# 开启docker远程api
 sudo sed -i 's/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/10.0.0.10:2375 -H fd:\/\//g' /lib/systemd/system/docker.service
 sudo systemctl daemon-reload
 sudo service docker restart
-#打印docker版本
-#docker version
-#安装docker-compose
+# 安装docker-compose
+# https://github.com/docker/compose
+# curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo cp /vagrant/lib/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
