0a47d64aea248667bf65fd968d190ded5cf43d55
xiaoyuit <1150601100@qq.com>
2019-04-30 09:58:48 +0800

    更新启动脚本

--- boot.sh
+++ boot.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo /usr/local/bin/docker-compose -f /vagrant/lnmp/docker-compose.yaml restart
+# 配置 docker-compose lnmp开机启动
+#sudo /usr/local/bin/docker-compose -f /vagrant/lnmp/docker-compose.yaml restart
