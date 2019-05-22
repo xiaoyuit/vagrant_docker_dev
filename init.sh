65729826c88b9391e4b35bd232f285788d6b8161
xiaoyuit <1150601100@qq.com>
2019-05-22 16:37:42 +0800

    更新初始化脚本

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 替换阿里云源
-sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g;s/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
+sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g;s/security.ubuntu.com/mirrors.aliyun.com/g;s/http:/https:/g' /etc/apt/sources.list
 # 开启密码登录并开启root账号登录
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g;s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo service ssh restart > /dev/null 2>&1
